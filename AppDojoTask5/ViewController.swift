c3d2c8f6c6cf640a96f7b50eeb27e3f96eaef472
akio0911 <akio0911@gmail.com>
2021-03-31 12:14:23 +0900

    feedback

--- AppDojoTask5/ViewController.swift
+++ AppDojoTask5/ViewController.swift
@@ -18,6 +18,17 @@ class ViewController: UIViewController {
 		case emptyLeftTextField
 		case emptyRightTextField
 		case divisionByZero
+
+        var message: String {
+            switch self {
+            case .emptyLeftTextField:
+                return "割られる数を入力してください"
+            case .emptyRightTextField:
+                return "割る数を入力してください"
+            case .divisionByZero:
+                return "割る数には0を入力しないでください"
+            }
+        }
 	}
 
 	override func viewDidLoad() {
@@ -29,18 +40,9 @@ class ViewController: UIViewController {
 
 	@IBAction private func tappedCalButton(_ sender: Any) {
 		do{
-			let result = try calField()
-			resultLabel.text = String(format: "%.5f",result)
+			resultLabel.text = String(format: "%.5f", try calField())
 		} catch let error as UserError {
-
-			switch error {
-			case .emptyLeftTextField:
-				showAlert(message: "割られる数を入力してください")
-			case .emptyRightTextField:
-				showAlert(message: "割る数を入力してください")
-			case .divisionByZero:
-				showAlert(message: "割る数には0を入力しないでください")
-			}
+            showAlert(message: error.message)
 		} catch {
 			print("その他のエラーです")
 		}
@@ -49,21 +51,20 @@ class ViewController: UIViewController {
 	// アラートを表示する関数
 	private func showAlert(message: String){
 		// OKボタンの処理
-		let oKAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel)
+		let oKAction = UIAlertAction(title: "OK", style: .cancel)
 
-		let alert: UIAlertController = UIAlertController(title: "課題5", message: message, preferredStyle:  UIAlertController.Style.alert)
-
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
 		alert.addAction(oKAction)
-		present(alert, animated: true, completion: nil)
 
+		present(alert, animated: true, completion: nil)
 	}
 
 	// 計算を行う関数
 	private func calField() throws -> Float{
-
 		guard !leftTextField.text!.isEmpty else{
 			throw UserError.emptyLeftTextField
 		}
+
 		guard !rightTextField.text!.isEmpty else {
 			throw UserError.emptyRightTextField
 		}
@@ -74,7 +75,7 @@ class ViewController: UIViewController {
 		guard value2 != 0 else {
 			throw UserError.divisionByZero
 		}
+
 		return Float(value1) / Float(value2)
 	}
 }
-
